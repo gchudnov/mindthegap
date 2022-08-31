76456e99e44226613c3a06b0322f6d0c2b3e9547
Grigorii Chudnov <g.chudnov@gmail.com>
2022-08-31 23:09:14 +0200

    upd intervals

--- src/main/scala/com/github/gchudnov/mtg/Interval.scala
+++ src/main/scala/com/github/gchudnov/mtg/Interval.scala
@@ -1,7 +1,9 @@
 package com.github.gchudnov.mtg
 
 /**
- * Generic Interval Representation
+ * Generic Interval Representation.
+ * 
+ * TODO: need to encode open/close ; half-open / half-closed intervals somehow
  */
 trait Interval[A: Ordering]:
   def x1: Option[A]
--- src/main/scala/com/github/gchudnov/mtg/Timeline.scala
+++ src/main/scala/com/github/gchudnov/mtg/Timeline.scala
@@ -1,4 +1,16 @@
 package com.github.gchudnov.mtg
 
+/**
+  * Timeline is a collection of intervals
+  * 
+  * Example:
+  * {{{
+  * 
+  *   [--)  [------]  (---->
+  * 
+  *  -----------------------------------------> t
+  * }}}
+  * 
+  */
 trait Timeline[A: Ordering]:
   def intervals: List[Interval[A]]
