94fbbc19b4829e70e9196be13a22436609e4ec22
Grigorii Chudnov <g.chudnov@gmail.com>
2022-09-01 10:06:12 +0200

    upd code

--- src/main/scala/com/github/gchudnov/mtg/Interval.scala
+++ src/main/scala/com/github/gchudnov/mtg/Interval.scala
@@ -1,10 +1,67 @@
 package com.github.gchudnov.mtg
 
 /**
- * Generic Interval Representation.
- * 
- * TODO: need to encode open/close ; half-open / half-closed intervals somehow
+ * Generic Interval Representation
+ *
+ * Classification of Intervals:
+ *   - Empty
+ *   - Degenerate
+ *   - Proper and Bounded
+ *      - Open
+ *      - Closed
+ *      - TODO: define
  */
-trait Interval[A: Ordering]:
-  def x1: Option[A]
-  def x2: Option[A]
+sealed trait Interval[T: Ordering]
+
+/**
+ * Empty:
+ * {{{
+ *   [b, a] = (b, a) = [b, a) = (b, a] = (a, a) = [a, a) = (a, a] = {} = ∅
+ * }}}
+ */
+object Empty extends Interval[Nothing]
+
+/**
+ * Degenerate
+ *
+ * Sonsists of a single real number
+ *
+ * {{{
+ *   [a, a] = {a}
+ * }}}
+ */
+final case class Degenerate[T: Ordering](a: T) extends Interval[T]
+
+/**
+ * Proper and Bounded
+ *
+ * Proper - an interval that is neither Empty nor Degenerate is said to be Proper.
+ *
+ * Bounded - an interval is said to be Bounded, if it is both left- and right-bounded; and is said to be Unbounded otherwise.
+ */
+
+/**
+ * Open - does not include its endpoints, and is indicated with parentheses, e.g. (0, 1).
+ *
+ * Proper and Bounded
+ *
+ * {{{
+ *   (a, b) = {x | a < x < b}
+ * }}}
+ */
+final case class Open[T: Ordering](a: T, b: T) extends Interval[T]
+
+/**
+ * Closed - an interval which includes all its limit points, e.g. [0, 1].
+ *
+ * Proper and Bounded
+ *
+ * {{{
+ *   [a, b] = {x | a <= x <= b}
+ * }}}
+ */
+final case class Closed[T: Ordering](a: T, b: T) extends Interval[T]
+
+/**
+ * HalfOpen - includes only one of its endpoints, e.g. (0, 1]. [0, 1).
+ */
--- src/test/scala/com/github/gchudnov/mtg/IntervalSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/IntervalSpec.scala
@@ -4,12 +4,14 @@ import java.time.Instant
 
 final class IntervalSpec extends TestSpec:
 
-  final case class InstantInterval(x1: Option[Instant], x2: Option[Instant]) extends Interval[Instant]
-  final case class IntInterval(x1: Option[Int], x2: Option[Int]) extends Interval[Int]
+  // final case class InstantInterval(x1: Option[Instant], x2: Option[Instant]) extends Interval[Instant]
+  // final case class IntInterval(x1: Option[Int], x2: Option[Int]) extends Interval[Int]
 
   "Interval" when {
     "IntInterval" should {
       "comparable" in {
+
+        // TODO: a collection of intervals can be added to a collection
 
 
         // val x = IntInterval(1, 2)
