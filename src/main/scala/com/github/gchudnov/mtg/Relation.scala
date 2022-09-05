fa9267f0a13f46c69ae75fadbbdc389deb6e947e
Grigorii Chudnov <g.chudnov@gmail.com>
2022-09-05 10:01:04 +0200

    upd code

--- src/main/scala/com/github/gchudnov/mtg/Relation.scala
+++ src/main/scala/com/github/gchudnov/mtg/Relation.scala
@@ -10,7 +10,7 @@ package com.github.gchudnov.mtg
  *   [AAA]              | A meets B               | (m)
  *       [BBB]          | B is-met-by A           | (M)
  *
- *   [AAA]              | A overlaps-with B       | (o)
+ *   [AAA]              | A overlaps B            | (o)
  *     [BBB]            | B is-overlapped-by A    | (O)
  *
  *   [AAA]              | A starts B              | (s)
@@ -109,35 +109,67 @@ object Relation:
     def isMetBy(b: Interval[T]): Boolean =
       b.meets(a)
 
-// /**
-//  * Overlaps (o)
-//  *
-//  * A overlaps-with B
-//  *
-//  * A o B
-//  *
-//  * {{{
-//  *  [AAA]
-//  *    [BBB]
-//  * }}}
-//  */
-// def overlapsWith[T: Ordering](a: Interval[T], b: Interval[T]): Boolean =
-//   ???
+    /**
+     * Overlaps (o)
+     *
+     * A overlaps B
+     *
+     * A <o> B
+     *
+     * {{{
+     *  [AAA]
+     *    [BBB]
+     * }}}
+     */
+    def overlaps(b: Interval[T]): Boolean =
+      (a, b) match
+        case (Proper(Some(x1), Some(x2), includeX1, includeX2), Proper(Some(y1), Some(y2), includeY1, includeY2)) =>
+          val ord = summon[Ordering[T]]
+          (ord.lt(x1, y1) || (ord.equiv(x1, y1) && includeX1 && !includeY1)) && 
+          (ord.lt(y1, x2) || (ord.equiv(y1, x2) && !includeX2 && includeY2))
+          ???
+        case (Proper(None, Some(x2), _, includeX2), Proper(Some(y1), Some(y2), includeY1, includeY2)) =>
+          val ord = summon[Ordering[T]]
+          (ord.lt(y1, x2)) && 
+          (ord.lt(x2, y2) || (ord.equiv(x2, y2) && !includeX2 && includeY2))
+        case (Proper(Some(x1), Some(x2), includeX1, includeX2), Proper(Some(y1), None, includeY1, _)) =>
+          val ord = summon[Ordering[T]]
+          (ord.lt(y1, x2)) && 
+          (ord.lt(x1, y1) || (ord.equiv(x1, y1) && includeX1 && !includeY1))          
+        case (Proper(None, Some(x2), _, includeX2), Proper(Some(y1), None, includeY1, _)) =>
+          val ord = summon[Ordering[T]]
+          ord.lt(y1, x2)          
+        case _ =>
+          false
 
-// /**
-//  * IsOverlapedBy (O)
-//  *
-//  * B is-overlapped-by A
-//  *
-//  * B O A
-//  *
-//  * {{{
-//  *  [AAA]
-//  *    [BBB]
-//  * }}}
-//  */
-// def isOverlapedBy[T: Ordering](b: Interval[T], a: Interval[T]): Boolean =
-//   overlapsWith(a, b)
+/*
+  def overlaps(other: TsTzRange): Boolean = {
+    // if any of the intervals are zero, return false early
+    !(this.zero || other.zero) &&
+      // if any of the intervals are infinite, return true early
+      (this.infinite || other.infinite) ||
+      // otherwise, depending on the boundedness of the 2 intervals, check accordingly
+      (this.unbounded && other.unbounded && doubleUnboundedCheck(this, other)) ||
+      (!this.unbounded && !other.unbounded && boundedCheck(this, other)) ||
+      (!this.unbounded && other.unbounded && singleUnboundedCheck(this, other)) ||
+      (this.unbounded && !other.unbounded && singleUnboundedCheck(other, this))
+  }
+*/
+
+    /**
+     * IsOverlapedBy (O)
+     *
+     * B is-overlapped-by A
+     *
+     * B <O> A
+     *
+     * {{{
+     *  [AAA]
+     *    [BBB]
+     * }}}
+     */
+    def isOverlapedBy(b: Interval[T]): Boolean =
+      b.overlaps(a)
 
 // /**
 //  * Starts (s)
@@ -243,3 +275,150 @@ object Relation:
 //  */
 // def equals[T: Ordering](a: Interval[T], b: Interval[T]): Boolean =
 //   ???
+
+
+
+
+// sealed abstract class Bounded[T: Ordering](a: T, b: T, isIncludeA: Boolean, isIncludeB: Boolean) extends Interval[T]
+
+// final case class Open[T: Ordering](a: T, b: T)                extends Bounded[T](a, b, isIncludeA = false, isIncludeB = false)
+// final case class Closed[T: Ordering](a: T, b: T)              extends Bounded[T](a, b, isIncludeA = true, isIncludeB = true)
+// final case class LeftClosedRightOpen[T: Ordering](a: T, b: T) extends Bounded[T](a, b, isIncludeA = true, isIncludeB = false)
+// final case class LeftOpenRightClosed[T: Ordering](a: T, b: T) extends Bounded[T](a, b, isIncludeA = false, isIncludeB = true)
+
+// sealed abstract class Unbounded[T: Ordering](a: Option[T], b: Option[T], isIncludeA: Boolean, isIncludeB: Boolean) extends Interval[T]
+
+// final case class LeftOpen[T: Ordering](a: T)   extends Unbounded[T](Some(a), None, isIncludeA = false, isIncludeB = false)
+// final case class LeftClosed[T: Ordering](a: T) extends Unbounded[T](Some(a), None, isIncludeA = true, isIncludeB = false)
+
+// final case class RightOpen[T: Ordering](b: T)   extends Unbounded[T](None, Some(b), isIncludeA = false, isIncludeB = false)
+// final case class RightClosed[T: Ordering](b: T) extends Unbounded[T](None, Some(b), isIncludeA = false, isIncludeB = true)
+
+/*   def isOpen: Boolean =
+    !isIncludeA && !isIncludeB
+
+  def isClosed: Boolean =
+    isIncludeA && isIncludeB
+
+  def isLeftClosedRightOpen: Boolean =
+    isIncludeA && !isIncludeB
+
+  def isLeftOpenRightClosed: Boolean =
+    !isIncludeA && isIncludeB
+ */
+
+// object Interval:
+
+// val empty: Empty.type =
+//   Empty
+
+// def degenerate[T: Ordering](a: T): Degenerate[T] =
+//   Degenerate[T](a)
+
+// def open[T: Ordering](a: T, b: T): Open[T] =
+//   Open[T](a = a, b = b)
+
+// def closed[T: Ordering](a: T, b: T): Closed[T] =
+//   Closed[T](a = a, b = b)
+
+// def leftClosedRightOpen[T: Ordering](a: T, b: T): LeftClosedRightOpen[T] =
+//   LeftClosedRightOpen[T](a = a, b = b)
+
+// def leftOpenRightClosed[T: Ordering](a: T, b: T): LeftOpenRightClosed[T] =
+//   LeftOpenRightClosed[T](a = a, b = b)
+
+/*
+import java.time.Instant
+import scala.math.Ordering.Implicits._
+case class TsTzRange(
+  start: Option[Instant],
+  end: Option[Instant],
+  startInclusive: Boolean = true,
+  endInclusive: Boolean = true) {
+
+  // if start and end are specified, start must be less than or equal to end
+  require((for { s <- start; e <- end} yield { s <= e}).getOrElse(true))
+
+  def zero: Boolean = this.bounded && this.start == this.end && !this.startInclusive || !this.endInclusive
+
+  def infinite: Boolean = this.start.isEmpty && this.end.isEmpty
+
+  // unbounded in this context refers to an interval that is only unbounded on one side
+  def unbounded: Boolean = leftUnbounded || rightUnbounded
+
+  def leftUnbounded: Boolean = this.start.isEmpty && this.end.nonEmpty
+
+  def rightUnbounded: Boolean = this.start.nonEmpty && this.end.isEmpty
+
+  def bounded: Boolean = this.start.nonEmpty && this.end.nonEmpty
+
+  private def touchingEdges(left: TsTzRange, right: TsTzRange): Boolean =
+    left.end == right.start && left.endInclusive && right.startInclusive
+
+  // Checks if the end of bounded2 lies anywhere within bounded1
+  private def boundedCheckLeftOverlap(bounded1: TsTzRange, bounded2: TsTzRange): Boolean =
+    bounded2.end > bounded1.start && bounded2.end <= bounded1.end
+
+  // Checks if the start of bounded2 lies anywhere within bounded1
+  private def boundedCheckRightOverlap(bounded1: TsTzRange, bounded2: TsTzRange): Boolean =
+    bounded2.start >= bounded1.start && bounded2.start < bounded1.end
+
+  // boundedCheck assumes both intervals are bounded
+  private def boundedCheck(bounded1: TsTzRange, bounded2: TsTzRange): Boolean =
+    boundedCheckLeftOverlap(bounded1, bounded2) ||
+      boundedCheckRightOverlap(bounded1, bounded2) ||
+      touchingEdges(bounded2, bounded1) ||
+      touchingEdges(bounded1, bounded2)
+
+  // checks if both ranges are unbounded in opposite directions, and check overlap if so
+  private def oppositeUnboundedCheck(leftUnbounded: TsTzRange, rightUnbounded: TsTzRange): Boolean =
+    leftUnbounded.leftUnbounded &&
+      rightUnbounded.rightUnbounded &&
+      (leftUnbounded.end > rightUnbounded.start || touchingEdges(leftUnbounded, rightUnbounded))
+
+  // checks if both ranges are unbounded in the same direction
+  private def sameUnboundedCheck(unbounded1: TsTzRange, unbounded2: TsTzRange): Boolean =
+    (unbounded1.leftUnbounded && unbounded2.leftUnbounded) ||
+      (unbounded1.rightUnbounded && unbounded2.rightUnbounded)
+
+  private def doubleUnboundedCheck(unbounded1: TsTzRange, unbounded2: TsTzRange): Boolean =
+    sameUnboundedCheck(unbounded1, unbounded2) ||
+      oppositeUnboundedCheck(unbounded1, unbounded2) ||
+      oppositeUnboundedCheck(unbounded2, unbounded1)
+
+  private def singleUnboundedLeftUnboundedCheck(bounded: TsTzRange, leftUnbounded: TsTzRange): Boolean =
+    leftUnbounded.leftUnbounded && leftUnbounded.end > bounded.start
+
+  private def singleUnboundedRightUnboundedCheck(bounded: TsTzRange, rightUnbounded: TsTzRange): Boolean =
+    rightUnbounded.rightUnbounded && rightUnbounded.start < bounded.end
+
+  // if only one of the intervals is bounded
+  private def singleUnboundedCheck(bounded: TsTzRange, unbounded: TsTzRange): Boolean =
+    singleUnboundedLeftUnboundedCheck(bounded, unbounded) ||
+      singleUnboundedRightUnboundedCheck(bounded, unbounded) ||
+      touchingEdges(bounded, unbounded) ||
+      touchingEdges(unbounded, bounded)
+
+  def overlaps(other: TsTzRange): Boolean = {
+    // if any of the intervals are zero, return false early
+    !(this.zero || other.zero) &&
+      // if any of the intervals are infinite, return true early
+      (this.infinite || other.infinite) ||
+      // otherwise, depending on the boundedness of the 2 intervals, check accordingly
+      (this.unbounded && other.unbounded && doubleUnboundedCheck(this, other)) ||
+      (!this.unbounded && !other.unbounded && boundedCheck(this, other)) ||
+      (!this.unbounded && other.unbounded && singleUnboundedCheck(this, other)) ||
+      (this.unbounded && !other.unbounded && singleUnboundedCheck(other, this))
+  }
+
+  def contains(other: Instant): Boolean = {
+    overlaps(TsTzRange(Some(other), Some(other)))
+  }
+
+  // returns true if it overlaps with the current time
+  def isCurrent: Boolean = {
+    this.contains(Instant.now)
+  }
+
+}
+ */
