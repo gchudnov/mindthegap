3fc9657a26e1b19c4b776428a58cc28e9a04a252
Grigorii Chudnov <g.chudnov@gmail.com>
2022-09-18 19:11:46 +0200

    upd code

--- src/test/scala/com/github/gchudnov/mtg/RelationSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/RelationSpec.scala
@@ -57,71 +57,71 @@ given OptionPartialOrdering[T: Ordering]: PartialOrdering[Option[T]] with
 
   "Relation" when {
 
-//     /**
-//      * Before, After (Preceeds, IsPreceededBy)
-//      *
-//      * {{{
-//      *   AAA
-//      *        BBB
-//      * }}}
-//      */
-//     "before (preceeds) & after (isPreceededBy)" should {
-//       "check" in {
-//         forAll(genOneIntTuple, genOneIntTuple) { case (((ox1, ox2), ix1, ix2), ((oy1, oy2), iy1, iy2)) =>
-//           val xx = Interval.make(ox1, ox2, ix1, ix2)
-//           val yy = Interval.make(oy1, oy2, iy1, iy2)
+    /**
+     * Before, After (Preceeds, IsPreceededBy)
+     *
+     * {{{
+     *   AAA
+     *        BBB
+     * }}}
+     */
+    "before (preceeds) & after (isPreceededBy)" should {
+      // "check" in {
+      //   forAll(genOneIntTuple, genOneIntTuple) { case (((ox1, ox2), ix1, ix2), ((oy1, oy2), iy1, iy2)) =>
+      //     val xx = Interval.make(ox1, ox2, ix1, ix2)
+      //     val yy = Interval.make(oy1, oy2, iy1, iy2)
 
-//           whenever(xx.preceeds(yy)) {
-//             // println(s"p: ${(xx, yy)}")
+      //     whenever(xx.preceeds(yy)) {
+      //       // println(s"p: ${(xx, yy)}")
 
-//             assertRelation("b", xx, yy)
-//             assertOneRelation(xx, yy)
+      //       assertRelation("b", xx, yy)
+      //       assertOneRelation(xx, yy)
 
-//             val isX2ltY1 = pOrd.lt(ox2, oy1)
-//             val isX2eqY1 = pOrd.equiv(ox2, oy1)
+      //       val isX2ltY1 = pOrd.lt(ox2, oy1)
+      //       val isX2eqY1 = pOrd.equiv(ox2, oy1)
 
-//             (isX2ltY1 || (isX2eqY1 && !(ix2 && iy1))) mustBe (true)
-//           }
-//         }
-//       }
+      //       (isX2ltY1 || (isX2eqY1 && !(ix2 && iy1))) mustBe (true)
+      //     }
+      //   }
+      // }
 
-//       "check edge cases" in {
-//         // Empty
-//         Interval.empty[Int].before(Interval.empty[Int]) mustBe (false)
-//         Interval.empty[Int].before(Interval.degenerate(1)) mustBe (false)
-//         Interval.empty[Int].before(Interval.closed(1, 2)) mustBe (false)
-//         Interval.empty[Int].before(Interval.open(1, 2)) mustBe (false)
-//         Interval.empty[Int].before(Interval.unbounded[Int]) mustBe (false)
+      "check edge cases" in {
+        // Empty
+        Interval.empty[Int].before(Interval.empty[Int]) mustBe (false)
+        Interval.empty[Int].before(Interval.degenerate(1)) mustBe (false)
+        Interval.empty[Int].before(Interval.closed(1, 2)) mustBe (false)
+        Interval.empty[Int].before(Interval.open(1, 2)) mustBe (false)
+        Interval.empty[Int].before(Interval.unbounded[Int]) mustBe (false)
 
-//         // Degenerate (Point)
-//         Interval.degenerate(5).before(Interval.empty[Int]) mustBe (false)
-//         Interval.degenerate(5).before(Interval.degenerate(5)) mustBe (false)
-//         Interval.degenerate(5).before(Interval.degenerate(10)) mustBe (true)
-//         Interval.degenerate(5).before(Interval.open(5, 10)) mustBe (true)
-//         Interval.degenerate(5).before(Interval.closed(5, 10)) mustBe (false)
-//         Interval.degenerate(5).before(Interval.closed(6, 10)) mustBe (true)
-//         Interval.degenerate(5).before(Interval.leftClosed(5)) mustBe (false)
-//         Interval.degenerate(5).before(Interval.leftClosed(6)) mustBe (true)
-//         Interval.degenerate(5).before(Interval.unbounded[Int]) mustBe (false)
+        // Degenerate (Point)
+        Interval.degenerate(5).before(Interval.empty[Int]) mustBe (false)
+        Interval.degenerate(5).before(Interval.degenerate(5)) mustBe (false)
+        Interval.degenerate(5).before(Interval.degenerate(10)) mustBe (true)
+        Interval.degenerate(5).before(Interval.open(5, 10)) mustBe (true)
+        Interval.degenerate(5).before(Interval.closed(5, 10)) mustBe (false)
+        Interval.degenerate(5).before(Interval.closed(6, 10)) mustBe (true)
+        Interval.degenerate(5).before(Interval.leftClosed(5)) mustBe (false)
+        Interval.degenerate(5).before(Interval.leftClosed(6)) mustBe (true)
+        Interval.degenerate(5).before(Interval.unbounded[Int]) mustBe (false)
 
-//         // Proper
-//         Interval.open(1, 2).before(Interval.empty[Int]) mustBe (false)
-//         Interval.open(1, 2).preceeds(Interval.open(5, 6)) mustBe (true)
-//         Interval.open(5, 6).isPreceededBy(Interval.open(1, 2)) mustBe (true)
-//         Interval.open(1, 2).preceeds(Interval.open(2, 3)) mustBe (true)
-//         Interval.open(1, 2).preceeds(Interval.closed(5, 6)) mustBe (true)
+        // Proper
+        Interval.open(1, 2).before(Interval.empty[Int]) mustBe (false)
+        Interval.open(1, 2).preceeds(Interval.open(5, 6)) mustBe (true)
+        Interval.open(5, 6).isPreceededBy(Interval.open(1, 2)) mustBe (true)
+        Interval.open(1, 2).preceeds(Interval.open(2, 3)) mustBe (true)
+        Interval.open(1, 2).preceeds(Interval.closed(5, 6)) mustBe (true)
 
-//         // Infinity
-//         // (1, 2)  (3, +inf)
-//         Interval.open(1, 2).before(Interval.leftOpen(3)) mustBe (true)
+        // Infinity
+        // (1, 2)  (3, +inf)
+        Interval.open(1, 2).before(Interval.leftOpen(3)) mustBe (true)
 
-//         // (-inf, 2)  (3, 5)
-//         Interval.rightOpen(2).before(Interval.open(3, 5)) mustBe (true)
+        // (-inf, 2)  (3, 5)
+        Interval.rightOpen(2).before(Interval.open(3, 5)) mustBe (true)
 
-//         // (-inf, 2)  (3, +inf)
-//         Interval.rightOpen(2).before(Interval.leftOpen(3)) mustBe (true)
-//       }
-//     }
+        // (-inf, 2)  (3, +inf)
+        Interval.rightOpen(2).before(Interval.leftOpen(3)) mustBe (true)
+      }
+    }
 
 //     /**
 //      * Meets, IsMetBy
