ea8a20e27a0c778edd5f7dfd2bb3bf521b0b8b06
Grigorii Chudnov <g.chudnov@gmail.com>
2022-09-18 17:18:18 +0200

    upd code

--- src/test/scala/com/github/gchudnov/mtg/internal/BoundaryOrderingSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/internal/BoundaryOrderingSpec.scala
@@ -11,8 +11,41 @@ final class BoundaryOrderingSpec extends TestSpec:
   "BoundaryOrdering" when {
     "(LeftBoundary, LeftBoundary)" should {
       "compare" in {
-        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](Some(1), isInclude = true)) mustBe(0)
+        // 1, 1
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (0)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (0)
 
+        // 1, 2
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](Some(2), isInclude = true)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](Some(2), isInclude = true)) mustBe (0)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](Some(2), isInclude = false)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](Some(2), isInclude = false)) mustBe (-1)
+
+        // 2, 1
+        ord.compare(LeftBoundary[Int](Some(2), isInclude = true), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(2), isInclude = false), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(2), isInclude = true), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (0)
+        ord.compare(LeftBoundary[Int](Some(2), isInclude = false), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (1)
+
+        // 1, -inf
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](None, isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = true), LeftBoundary[Int](None, isInclude = false)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](None, isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](Some(1), isInclude = false), LeftBoundary[Int](None, isInclude = false)) mustBe (1)
+
+        // -inf, 1
+        ord.compare(LeftBoundary[Int](None, isInclude = true), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](None, isInclude = true), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](None, isInclude = false), LeftBoundary[Int](Some(1), isInclude = true)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](None, isInclude = false), LeftBoundary[Int](Some(1), isInclude = false)) mustBe (-1)
+
+        // -inf, -inf
+        ord.compare(LeftBoundary[Int](None, isInclude = true), LeftBoundary[Int](None, isInclude = true)) mustBe (0)
+        ord.compare(LeftBoundary[Int](None, isInclude = true), LeftBoundary[Int](None, isInclude = false)) mustBe (-1)
+        ord.compare(LeftBoundary[Int](None, isInclude = false), LeftBoundary[Int](None, isInclude = true)) mustBe (1)
+        ord.compare(LeftBoundary[Int](None, isInclude = false), LeftBoundary[Int](None, isInclude = false)) mustBe (0)
       }
     }
   }
