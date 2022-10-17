c508d98124addeaa4c22c986b9e90abfe5682e05
Grigorii Chudnov <g.chudnov@gmail.com>
2022-10-18 00:39:15 +0200

    diagram impl

--- src/main/scala/com/github/gchudnov/mtg/Diagram.scala
+++ src/main/scala/com/github/gchudnov/mtg/Diagram.scala
@@ -1,16 +1,15 @@
 package com.github.gchudnov.mtg
 
-import Diagram.*
-import Show.*
+import Show.given
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.ListBuffer
 
 final case class Diagram(
   width: Int,
   height: Int,
-  spans: List[Span],
-  ticks: List[Tick],
-  labels: List[Label],
+  spans: List[Diagram.Span],
+  ticks: List[Diagram.Tick],
+  labels: List[Diagram.Label],
   legend: List[String]
 )
 
@@ -55,21 +54,45 @@ object Diagram:
         label = Label.None
       )
 
+  final case class View[+T: Numeric](
+    left: Option[T], // left boundary of the view
+    right: Option[T] // right boundary of the view
+  ):
+    require(
+      left.flatMap(lhs => right.map(rhs => summon[Ordering[T]].lteq(lhs, rhs))).getOrElse(true),
+      "left view boundary must be less or equal to right view boundary"
+    )
+
+    val width: Option[T] =
+      for
+        lhs <- left
+        rhs <- right
+        dx  <- if summon[Ordering[T]].lt(lhs, rhs) then Some(summon[Numeric[T]].minus(rhs, lhs)) else None
+      yield dx
+
+  object View:
+    def default[T: Numeric]: View[T] =
+      View(
+        left = None,
+        right = None
+      )
+
   final case class Canvas[+T](
     width: Int,
-    padding: Int,
-    left: Option[T], // left boundary
-    right: Option[T] // right boundary
-  )
+    padding: Int
+  ):
+    val left: Int   = 0               // start of the canvas, inclusive
+    val right: Int  = width - 1       // end of the canvas, inclusive
+    val first: Int  = left + padding  // first offset for non-inf value
+    val last: Int   = right - padding // laft offset for non-inf value
+    val spread: Int = last - first
 
   object Canvas:
 
     val small: Canvas[Nothing] =
       Canvas[Nothing](
         width = 40,
-        padding = 2,
-        left = None,
-        right = None
+        padding = 2
       )
 
   final case class Tick(pos: Int)
@@ -97,87 +120,84 @@ object Diagram:
   /**
    * Prepare intervals for the rendering
    */
-  def prepare[T: Numeric](intervals: List[Interval[T]], canvas: Canvas[T])(using bOrd: Ordering[Boundary[T]]): Diagram =
+  def prepare[T: Domain: Numeric](intervals: List[Interval[T]], view: View[T], canvas: Canvas[T])(using Ordering[Boundary[T]]): Diagram =
     val tNum = summon[Numeric[T]]
 
-    val xs = intervals.filter(_.nonEmpty)
-    if xs.isEmpty then Diagram.empty
-    else
-      val bs: List[Boundary[T]] = xs.flatMap(it => List(it.left, it.right))
+    val xs: List[Interval[T]] = intervals.filter(_.nonEmpty)
+    val bs: List[Boundary[T]] = xs.flatMap(it => List[Boundary[T]](it.left, it.right)).filter(_.isBounded)
 
-      val ofMin = canvas.left.fold(bs.filter(_.isBounded).minOption.flatMap(_.value))(Some(_))
-      val ofMax = canvas.right.fold(bs.filter(_.isBounded).maxOption.flatMap(_.value))(Some(_))
+    // effective view that displayed on the canbas
+    val effectiveView = View(
+      left = view.left.fold(bs.minOption.flatMap(_.value))(Some(_)),
+      right = view.right.fold(bs.maxOption.flatMap(_.value))(Some(_))
+    )
 
-//       val cxFirst = 0
-//       val cxLast  = canvas.width - 1
-//       val cxMin   = cxFirst + canvas.padding
-//       val cxMax   = cxLast - canvas.padding
-//       val cw      = cxMax - cxMin
+    // val ok = ofw.map(fw => cw.toDouble / tNum.toDouble(fw))
 
-//       val ofw = for
-//         fMin <- ofMin
-//         fMax <- ofMax
-//         dx   <- if !tNum.equiv(fMax, fMin) then Some(tNum.minus(fMax, fMin)) else None
-//       yield dx
+    // // translates the coordindate into position on the canvas
+    // def translateX(value: Option[T], isLeft: Boolean): Int =
+    //   value match
+    //     case None =>
+    //       // -inf or +inf
+    //       if isLeft then cxFirst else cxLast
+    //     case Some(x) =>
+    //       ok match
+    //         case None =>
+    //           // one of the boundaries is inf
+    //           if isLeft then cxMin else cxMax
+    //         case Some(k) =>
+    //           // finite boundaries
+    //           val fMin = ofMin.get
+    //           val df   = tNum.minus(x, fMin)
+    //           val dd   = tNum.toDouble(df)
+    //           align((k * dd) + cxMin)
 
-//       val ok = ofw.map(fw => cw.toDouble / tNum.toDouble(fw))
+    // /**
+    //  * Calculate Label position; Try to align only if close to ends of the canvas
+    //  */
+    // def toLabelPos(x: Int, text: String): Int =
+    //   val p = align(x - (text.size / 2.0))
+    //   val q = p + text.size
 
-//       // translates the coordindate into position on the canvas
-//       def translateX(value: Option[T], isLeft: Boolean): Int =
-//         value match
-//           case None =>
-//             // -inf or +inf
-//             if isLeft then cxFirst else cxLast
-//           case Some(x) =>
-//             ok match
-//               case None =>
-//                 // one of the boundaries is inf
-//                 if isLeft then cxMin else cxMax
-//               case Some(k) =>
-//                 // finite boundaries
-//                 val fMin = ofMin.get
-//                 val df   = tNum.minus(x, fMin)
-//                 val dd   = tNum.toDouble(df)
-//                 align((k * dd) + cxMin)
+    //   if (p < 0 && (x >= 0 && x < canvas.width)) then 0
+    //   else if (q >= canvas.width && (x >= 0 && x < canvas.width)) then canvas.width - text.size
+    //   else p
 
-//       def centerX(value: Option[T]): Int =
-//         align(cxMin + (cw / 2.0))
+    // xs.foldLeft(Diagram.empty) { case (acc, i) =>
+    //   val y = acc.height
 
-//       /**
-//        * Calculate Label position; Try to align only if close to ends of the canvas
-//        */
-//       def toLabelPos(x: Int, text: String): Int =
-//         val p = align(x - (text.size / 2.0))
-//         val q = p + text.size
+    //   // spans
+    //   val x0   = translateX(i.left.value, isLeft = true)
+    //   val x1   = if i.isProper then translateX(i.right.value, isLeft = false) else x0
+    //   val span = Span(x0 = x0, x1 = x1, y = y, includeX0 = i.left.isInclude, includeX1 = i.right.isInclude)
 
-//         if (p < 0 && (x >= 0 && x < canvas.width)) then 0
-//         else if (q >= canvas.width && (x >= 0 && x < canvas.width)) then canvas.width - text.size
-//         else p
+    //   // ticks, labels
+    //   val x0Text  = Show.leftValue(i.left.value)
+    //   val x0Pos   = toLabelPos(x0, x0Text)
+    //   val x0Tick  = Tick(x0)
+    //   val x0Label = Label(pos = x0Pos, value = x0Text)
 
-//       val diagram = xs.foldLeft(Diagram.empty) { case (acc, i) =>
-//         val y = acc.height
+    //   val x1Text  = Show.rightValue(i.right.value)
+    //   val x1Pos   = toLabelPos(x1, x1Text)
+    //   val x1Tick  = Tick(x1)
+    //   val x1Label = Label(pos = x1Pos, value = x1Text)
 
-//         // spans
-//         val x0   = translateX(i.left.value, isLeft = true)
-//         val x1   = if i.isProper then translateX(i.right.value, isLeft = false) else x0
-//         val span = Span(x0 = x0, x1 = x1, y = y, includeX0 = i.left.isInclude, includeX1 = i.right.isInclude)
+    //   val ticks  = List(x0Tick, x1Tick)
+    //   val labels = List(x0Label, x1Label)
 
-//         // labels
-//         val x0Text = Show.leftValue(i.left.value)
-//         val x0Pos  = toLabelPos(x0, x0Text)
-//         val x1Text = Show.rightValue(i.right.value)
-//         val x1Pos  = toLabelPos(x1, x1Text)
-//         val labels = List(Label(tick = x0, pos = x0Pos, value = x0Text), Label(tick = x1, pos = x1Pos, value = x1Text))
+    //   val iLegend = i.show
 
-//         // legend
-//         val iLegend = i.show
+    //   acc.copy(
+    //     width = canvas.width,
+    //     height = y + 1,
+    //     spans = acc.spans :+ span,
+    //     ticks = (acc.ticks ++ ticks).distinct,
+    //     labels = (acc.labels ++ labels).distinct,
+    //     legend = acc.legend :+ iLegend
+    //   )
+    // }
 
-//         acc.copy(height = y + 1, spans = acc.spans :+ span, labels = acc.labels ++ labels, legend = acc.legend :+ iLegend)
-//       }
-
-//       diagram.copy(width = canvas.width, labels = diagram.labels.distinct)
-
-      ???
+    ???
 
 //   /**
 //    * Render the provided Diagram
@@ -248,11 +268,11 @@ object Diagram:
 //   private def clap(value: Double, minValue: Double, maxValue: Double): Double =
 //     if value < minValue then minValue else if value > maxValue then maxValue else value
 
-//   /**
-//    * Align value to the grid
-//    */
-//   private def align(value: Double): Int =
-//     value.round.toInt
+  /**
+   * Align value to the grid
+   */
+  private def align(value: Double): Int =
+    value.round.toInt
 
 //   /**
 //    * Measure the number of lines required to draw all labels
--- src/test/scala/com/github/gchudnov/mtg/DiagramSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/DiagramSpec.scala
@@ -1,9 +1,26 @@
 package com.github.gchudnov.mtg
 
+import com.github.gchudnov.mtg.Diagram.Canvas
+import com.github.gchudnov.mtg.Diagram.Theme
+import com.github.gchudnov.mtg.Diagram.Span
+import com.github.gchudnov.mtg.Diagram.Tick
+import com.github.gchudnov.mtg.Diagram.Label
+import com.github.gchudnov.mtg.Diagram.View
+
 final class DiagramSpec extends TestSpec:
+
+  private val canvas: Canvas[Int] = Canvas.small
+  private val view: View[Int]     = View.default
+  private val theme: Theme        = Theme.default
+
   "Diagram" when {
     "prepare" should {
 
-      "prepare a point" in {}
+      "prepare a point" in {
+        // val a       = Interval.degenerate[Int](5) // [5]
+        // val diagram = Diagram.prepare(List(a), view, canvas)
+
+        // diagram mustBe Diagram(40, 1, List(Span(2, 2, 0, true, true)), List(Tick(2)), List(Label(2, "5")), List("{5}"))
+      }
     }
   }
