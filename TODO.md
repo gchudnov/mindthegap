4c30df57fdd70390d09d8e8755c8998fc165ac04
Grigorii Chudnov <g.chudnov@gmail.com>
2022-10-26 19:07:26 +0200

    add test

--- TODO.md
+++ TODO.md
@@ -5,3 +5,4 @@
 
 - add COUNT -- or size to get the cardinality, add it to Domain (Ordinal)
 - - canonical - add a method to convert to [) interval
+- add more operators and law tests
---
+++ src/test/scala/com/github/gchudnov/mtg/LawsSpec.scala
@@ -0,0 +1,28 @@
+package com.github.gchudnov.mtg
+
+import com.github.gchudnov.mtg.Arbitraries.*
+import com.github.gchudnov.mtg.Interval
+import com.github.gchudnov.mtg.TestSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.*
+
+final class LawsSpec extends TestSpec:
+
+  given intRange: IntRange = intRange5
+  given intProb: IntProb   = intProb127
+
+  given config: PropertyCheckConfiguration = PropertyCheckConfiguration(maxDiscardedFactor = 1000.0)
+
+  "Laws" when {
+    "a.intersection(b) is defined" should {
+      "a.UNION(b) is certainly defined as well" in {
+        forAll(genOneOfIntArgs, genOneOfIntArgs) { case (((ox1, ix1), (ox2, ix2)), ((oy1, iy1), (oy2, iy2))) =>
+          val xx = Interval.make(ox1, ix1, ox2, ix2)
+          val yy = Interval.make(oy1, iy1, oy2, iy2)
+
+          whenever(xx.intersection(yy).nonEmpty) {
+            xx.union(yy).nonEmpty mustBe true
+          }
+        }
+      }
+    }
+  }
