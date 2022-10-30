3cdd31d1690066136e6500be1c801848c879584e
Grigorii Chudnov <g.chudnov@gmail.com>
2022-10-30 16:24:57 +0100

    migrate before tests

--- src/test/scala/com/github/gchudnov/mtg/internal/BeforeSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/internal/BeforeSpec.scala
@@ -1,9 +1,10 @@
 package com.github.gchudnov.mtg.internal
 
 import com.github.gchudnov.mtg.Arbitraries.*
-import com.github.gchudnov.mtg.Mark
 import com.github.gchudnov.mtg.Interval
+import com.github.gchudnov.mtg.Mark
 import com.github.gchudnov.mtg.TestSpec
+import com.github.gchudnov.mtg.Value
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.*
 
 /**
@@ -21,117 +22,117 @@ final class BeforeSpec extends TestSpec:
 
   given config: PropertyCheckConfiguration = PropertyCheckConfiguration(maxDiscardedFactor = 1000.0)
 
-  // val ordM: Ordering[Mark[Int]] = summon[Ordering[Mark[Int]]]
+  val ordM: Ordering[Mark[Int]] = summon[Ordering[Mark[Int]]]
 
   "Before" when {
-    // import IntervalRelAssert.*
+    import IntervalRelAssert.*
 
-    // "a.before(b)" should {
-    //   "b.after(a)" in {
-    //     forAll(genOneOfIntArgs, genOneOfIntArgs) { case (((ox1, ix1), (ox2, ix2)), ((oy1, iy1), (oy2, iy2))) =>
-    //       val xx = Interval.make(ox1, ix1, ox2, ix2)
-    //       val yy = Interval.make(oy1, iy1, oy2, iy2)
+    "a.before(b)" should {
+      "b.after(a)" in {
+        forAll(genOneOfIntArgs, genOneOfIntArgs) { case (argsX, argsY) =>
+          val xx = Interval.make(argsX.left, argsX.right)
+          val yy = Interval.make(argsY.left, argsY.right)
 
-    //       whenever(xx.before(yy)) {
-    //         yy.after(xx) mustBe true
+          whenever(xx.before(yy)) {
+            yy.after(xx) mustBe true
 
-    //         assertOne(Rel.Before)(xx, yy)
+            assertOne(Rel.Before)(xx, yy)
 
-    //         // a+ < b-
-    //         val a2 = Mark.Right(ox2, ix2)
-    //         val b1 = Mark.Left(oy1, iy1)
+            // a+ < b-
+            val a2 = argsX.right
+            val b1 = argsY.left
 
-    //         ordM.lt(a2, b1) mustBe true
-    //       }
-    //     }
-    //   }
-    // }
+            ordM.lt(a2, b1) mustBe true
+          }
+        }
+      }
+    }
 
-    // "a.after(b)" should {
-    //   "b.before(a)" in {
-    //     forAll(genOneOfIntArgs, genOneOfIntArgs) { case (((ox1, ix1), (ox2, ix2)), ((oy1, iy1), (oy2, iy2))) =>
-    //       val xx = Interval.make(ox1, ix1, ox2, ix2)
-    //       val yy = Interval.make(oy1, iy1, oy2, iy2)
+    "a.after(b)" should {
+      "b.before(a)" in {
+        forAll(genOneOfIntArgs, genOneOfIntArgs) { case (argsX, argsY) =>
+          val xx = Interval.make(argsX.left, argsX.right)
+          val yy = Interval.make(argsY.left, argsY.right)
 
-    //       whenever(xx.after(yy)) {
-    //         yy.before(xx) mustBe true
+          whenever(xx.after(yy)) {
+            yy.before(xx) mustBe true
 
-    //         assertOne(Rel.After)(xx, yy)
+            assertOne(Rel.After)(xx, yy)
 
-    //         // a- > b+
-    //         val a1 = Mark.Left(ox1, ix1)
-    //         val b2 = Mark.Right(oy2, iy2)
+            // a- > b+
+            val a1 = argsX.left
+            val b2 = argsY.right
 
-    //         ordM.gt(a1, b2) mustBe true
-    //       }
-    //     }
-    //   }
-    // }
+            ordM.gt(a1, b2) mustBe true
+          }
+        }
+      }
+    }
 
-    // "a.before(b) AND b.after(a)" should {
+    "a.before(b) AND b.after(a)" should {
 
-    //   "equal" in {
-    //     forAll(genOneOfIntArgs, genOneOfIntArgs) { case (((ox1, ix1), (ox2, ix2)), ((oy1, iy1), (oy2, iy2))) =>
-    //       val xx = Interval.make(ox1, ix1, ox2, ix2)
-    //       val yy = Interval.make(oy1, iy1, oy2, iy2)
+      "equal" in {
+        forAll(genOneOfIntArgs, genOneOfIntArgs) { case (argsX, argsY) =>
+          val xx = Interval.make(argsX.left, argsX.right)
+          val yy = Interval.make(argsY.left, argsY.right)
 
-    //       val actual   = xx.before(yy)
-    //       val expected = yy.after(xx)
+          val actual   = xx.before(yy)
+          val expected = yy.after(xx)
 
-    //       actual mustBe expected
-    //     }
-    //   }
+          actual mustBe expected
+        }
+      }
 
-    //   "valid in special cases" in {
-    //     // Empty
-    //     Interval.empty[Int].before(Interval.empty[Int]) mustBe (false)
-    //     Interval.empty[Int].before(Interval.point(1)) mustBe (false)
-    //     Interval.empty[Int].before(Interval.closed(1, 4)) mustBe (false)
-    //     Interval.empty[Int].before(Interval.open(1, 4)) mustBe (false)
-    //     Interval.empty[Int].before(Interval.unbounded[Int]) mustBe (false)
+      "valid in special cases" in {
+        // Empty
+        Interval.empty[Int].before(Interval.empty[Int]) mustBe (false)
+        Interval.empty[Int].before(Interval.point(1)) mustBe (false)
+        Interval.empty[Int].before(Interval.closed(1, 4)) mustBe (false)
+        Interval.empty[Int].before(Interval.open(1, 4)) mustBe (false)
+        Interval.empty[Int].before(Interval.unbounded[Int]) mustBe (false)
 
-    //     // Point
-    //     Interval.point(5).before(Interval.empty[Int]) mustBe (false)
-    //     Interval.point(5).before(Interval.point(5)) mustBe (false)
-    //     Interval.point(5).before(Interval.point(6)) mustBe (true)
-    //     Interval.point(6).after(Interval.point(5)) mustBe (true)
-    //     Interval.point(5).before(Interval.point(10)) mustBe (true)
-    //     Interval.point(5).before(Interval.open(5, 10)) mustBe (true)
-    //     Interval.point(5).before(Interval.closed(5, 10)) mustBe (false)
-    //     Interval.point(5).before(Interval.closed(6, 10)) mustBe (true)
-    //     Interval.point(5).before(Interval.leftClosed(5)) mustBe (false)
-    //     Interval.point(5).before(Interval.leftClosed(6)) mustBe (true)
-    //     Interval.point(5).before(Interval.unbounded[Int]) mustBe (false)
+        // Point
+        Interval.point(5).before(Interval.empty[Int]) mustBe (false)
+        Interval.point(5).before(Interval.point(5)) mustBe (false)
+        Interval.point(5).before(Interval.point(6)) mustBe (true)
+        Interval.point(6).after(Interval.point(5)) mustBe (true)
+        Interval.point(5).before(Interval.point(10)) mustBe (true)
+        Interval.point(5).before(Interval.open(5, 10)) mustBe (true)
+        Interval.point(5).before(Interval.closed(5, 10)) mustBe (false)
+        Interval.point(5).before(Interval.closed(6, 10)) mustBe (true)
+        Interval.point(5).before(Interval.leftClosed(5)) mustBe (false)
+        Interval.point(5).before(Interval.leftClosed(6)) mustBe (true)
+        Interval.point(5).before(Interval.unbounded[Int]) mustBe (false)
 
-    //     // [-∞,0], {4}
-    //     Interval.point(4).after(Interval.proper[Int](None, true, Some(0), true)) mustBe (true)
-    //     Interval.proper[Int](None, true, Some(0), true).before(Interval.point(4)) mustBe (true)
+        // [-∞,0], {4}
+        Interval.point(4).after(Interval.proper[Int](Mark.at(Value.infNeg), Mark.at(Value.finite(0)))) mustBe (true)
+        Interval.proper[Int](Mark.at(Value.infNeg), Mark.at(Value.finite(0))).before(Interval.point(4)) mustBe (true)
 
-    //     // Proper
-    //     Interval.open(4, 7).before(Interval.open(4, 7)) mustBe (false)
-    //     Interval.open(1, 4).before(Interval.empty[Int]) mustBe (false)
-    //     Interval.open(1, 4).before(Interval.open(5, 8)) mustBe (true)
-    //     Interval.open(5, 8).after(Interval.open(1, 4)) mustBe (true)
-    //     Interval.open(1, 4).before(Interval.closed(5, 6)) mustBe (true)
+        // Proper
+        Interval.open(4, 7).before(Interval.open(4, 7)) mustBe (false)
+        Interval.open(1, 4).before(Interval.empty[Int]) mustBe (false)
+        Interval.open(1, 4).before(Interval.open(5, 8)) mustBe (true)
+        Interval.open(5, 8).after(Interval.open(1, 4)) mustBe (true)
+        Interval.open(1, 4).before(Interval.closed(5, 6)) mustBe (true)
 
-    //     Interval.open(1, 4).before(Interval.open(3, 6)) mustBe (true)
-    //     Interval.open(3, 6).after(Interval.open(1, 4)) mustBe (true)
+        Interval.open(1, 4).before(Interval.open(3, 6)) mustBe (true)
+        Interval.open(3, 6).after(Interval.open(1, 4)) mustBe (true)
 
-    //     // Infinity
-    //     // (1, 4)  (3, +inf)
-    //     Interval.open(1, 4).before(Interval.leftOpen(3)) mustBe (true)
+        // Infinity
+        // (1, 4)  (3, +inf)
+        Interval.open(1, 4).before(Interval.leftOpen(3)) mustBe (true)
 
-    //     // (-inf, 2)  (3, 6)
-    //     Interval.rightOpen(2).before(Interval.open(3, 6)) mustBe (true)
+        // (-inf, 2)  (3, 6)
+        Interval.rightOpen(2).before(Interval.open(3, 6)) mustBe (true)
 
-    //     // (-inf, 2)  (3, +inf)
-    //     Interval.rightOpen(2).before(Interval.leftOpen(3)) mustBe (true)
+        // (-inf, 2)  (3, +inf)
+        Interval.rightOpen(2).before(Interval.leftOpen(3)) mustBe (true)
 
-    //     // (-inf, 2]  (3, +inf)
-    //     Interval.rightClosed(2).before(Interval.leftOpen(3)) mustBe (true)
+        // (-inf, 2]  (3, +inf)
+        Interval.rightClosed(2).before(Interval.leftOpen(3)) mustBe (true)
 
-    //     // (-inf, 2)  [3, +inf)
-    //     Interval.rightOpen(2).before(Interval.leftClosed(3)) mustBe (true)
-    //   }
-    // }
+        // (-inf, 2)  [3, +inf)
+        Interval.rightOpen(2).before(Interval.leftClosed(3)) mustBe (true)
+      }
+    }
   }
--- src/test/scala/com/github/gchudnov/mtg/internal/IntervalRelAssert.scala
+++ src/test/scala/com/github/gchudnov/mtg/internal/IntervalRelAssert.scala
@@ -1,91 +1,99 @@
 package com.github.gchudnov.mtg.internal
 
+import com.github.gchudnov.mtg.Domain
+import com.github.gchudnov.mtg.Interval
+import com.github.gchudnov.mtg.Mark
+import com.github.gchudnov.mtg.Show
 import com.github.gchudnov.mtg.Show.given
 import org.scalatest.matchers.must.Matchers.*
-import com.github.gchudnov.mtg.Domain
-import com.github.gchudnov.mtg.Mark
-import com.github.gchudnov.mtg.Interval
 
 trait IntervalRelAssert:
   import IntervalRelAssert.*
 
-  // private def relFnMap[T](using bOrd: Ordering[Mark[T]]) =
-  //   Map(
-  //     Rel.Before        -> ((xx: Interval[T], yy: Interval[T]) => xx.before(yy)),
-  //     Rel.After         -> ((xx: Interval[T], yy: Interval[T]) => xx.after(yy)),
-  //     Rel.Meets         -> ((xx: Interval[T], yy: Interval[T]) => xx.meets(yy)),
-  //     Rel.IsMetBy       -> ((xx: Interval[T], yy: Interval[T]) => xx.isMetBy(yy)),
-  //     Rel.Overlaps      -> ((xx: Interval[T], yy: Interval[T]) => xx.overlaps(yy)),
-  //     Rel.IsOverlapedBy -> ((xx: Interval[T], yy: Interval[T]) => xx.isOverlapedBy(yy)),
-  //     Rel.During        -> ((xx: Interval[T], yy: Interval[T]) => xx.during(yy)),
-  //     Rel.Contains      -> ((xx: Interval[T], yy: Interval[T]) => xx.contains(yy)),
-  //     Rel.Starts        -> ((xx: Interval[T], yy: Interval[T]) => xx.starts(yy)),
-  //     Rel.IsStartedBy   -> ((xx: Interval[T], yy: Interval[T]) => xx.isStartedBy(yy)),
-  //     Rel.Finishes      -> ((xx: Interval[T], yy: Interval[T]) => xx.finishes(yy)),
-  //     Rel.IsFinishedBy  -> ((xx: Interval[T], yy: Interval[T]) => xx.isFinishedBy(yy)),
-  //     Rel.EqualsTo      -> ((xx: Interval[T], yy: Interval[T]) => xx.equalsTo(yy))
-  //   )
+  private def relFnMap[T](using Ordering[Mark[T]]) =
+    Map(
+      Rel.Before        -> ((xx: Interval[T], yy: Interval[T]) => xx.before(yy)),
+      Rel.After         -> ((xx: Interval[T], yy: Interval[T]) => xx.after(yy)),
+      Rel.Meets         -> ((xx: Interval[T], yy: Interval[T]) => xx.meets(yy)),
+      Rel.IsMetBy       -> ((xx: Interval[T], yy: Interval[T]) => xx.isMetBy(yy)),
+      Rel.Overlaps      -> ((xx: Interval[T], yy: Interval[T]) => xx.overlaps(yy)),
+      Rel.IsOverlapedBy -> ((xx: Interval[T], yy: Interval[T]) => xx.isOverlapedBy(yy)),
+      Rel.During        -> ((xx: Interval[T], yy: Interval[T]) => xx.during(yy)),
+      Rel.Contains      -> ((xx: Interval[T], yy: Interval[T]) => xx.contains(yy)),
+      Rel.Starts        -> ((xx: Interval[T], yy: Interval[T]) => xx.starts(yy)),
+      Rel.IsStartedBy   -> ((xx: Interval[T], yy: Interval[T]) => xx.isStartedBy(yy)),
+      Rel.Finishes      -> ((xx: Interval[T], yy: Interval[T]) => xx.finishes(yy)),
+      Rel.IsFinishedBy  -> ((xx: Interval[T], yy: Interval[T]) => xx.isFinishedBy(yy)),
+      Rel.EqualsTo      -> ((xx: Interval[T], yy: Interval[T]) => xx.equalsTo(yy))
+    )
 
-  // private val invRels: Map[Rel, Rel] = Map(
-  //   // forward
-  //   Rel.Before   -> Rel.After,
-  //   Rel.Meets    -> Rel.IsMetBy,
-  //   Rel.Overlaps -> Rel.IsOverlapedBy,
-  //   Rel.During   -> Rel.Contains,
-  //   Rel.Starts   -> Rel.IsStartedBy,
-  //   Rel.Finishes -> Rel.IsFinishedBy,
-  //   // backward
-  //   Rel.After         -> Rel.Before,
-  //   Rel.IsMetBy       -> Rel.Meets,
-  //   Rel.IsOverlapedBy -> Rel.Overlaps,
-  //   Rel.Contains      -> Rel.During,
-  //   Rel.IsStartedBy   -> Rel.Starts,
-  //   Rel.IsFinishedBy  -> Rel.Finishes,
-  //   // dual
-  //   Rel.EqualsTo -> Rel.EqualsTo
-  // )
+  private val invRels: Map[Rel, Rel] = Map(
+    // forward
+    Rel.Before   -> Rel.After,
+    Rel.Meets    -> Rel.IsMetBy,
+    Rel.Overlaps -> Rel.IsOverlapedBy,
+    Rel.During   -> Rel.Contains,
+    Rel.Starts   -> Rel.IsStartedBy,
+    Rel.Finishes -> Rel.IsFinishedBy,
+    // backward
+    Rel.After         -> Rel.Before,
+    Rel.IsMetBy       -> Rel.Meets,
+    Rel.IsOverlapedBy -> Rel.Overlaps,
+    Rel.Contains      -> Rel.During,
+    Rel.IsStartedBy   -> Rel.Starts,
+    Rel.IsFinishedBy  -> Rel.Finishes,
+    // dual
+    Rel.EqualsTo -> Rel.EqualsTo
+  )
 
-  // /**
-  //  * Finds relations two itervals are satisfying
-  //  */
-  // def findRelations[T](xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Set[Rel] =
-  //   relFnMap[T].foldLeft(Set.empty[Rel]) { case (acc, (k, fn)) =>
-  //     val res = fn(xx, yy)
-  //     if res then acc + k
-  //     else acc
-  //   }
+  /**
+   * Finds relations two itervals are satisfying
+   */
+  def findRelations[T](xx: Interval[T], yy: Interval[T])(using Ordering[Mark[T]]): Set[Rel] =
+    relFnMap[T].foldLeft(Set.empty[Rel]) { case (acc, (k, fn)) =>
+      val res = fn(xx, yy)
+      if res then acc + k
+      else acc
+    }
 
-  // def assertAny[T](xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
-  //   val trues      = findRelations(xx, yy)
-  //   val isNonEmpty = !(xx.isEmpty || yy.isEmpty)
-  //   if isNonEmpty && trues.size != 1 then
-  //     fail(s"xx: ${xx}, yy: ${yy}: |${xx.show}, ${yy.show}| satisfies ${trues.size} relations: ${trues.mkString("[", ",", "]")}, expected only one relation")
+  def assertAny[T: Domain](xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
+    val trues      = findRelations(xx, yy)
+    val isNonEmpty = !(xx.isEmpty || yy.isEmpty)
+    if isNonEmpty && trues.size != 1 then
+      fail(s"xx: ${xx}, yy: ${yy}: |${Show.asString(xx)}, ${Show.asString(yy)}| satisfies ${trues.size} relations: ${trues.mkString("[", ",", "]")}, expected only one relation")
 
-  // def assertOneOf[T](rs: Set[Rel])(xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
-  //   val trues = findRelations(xx, yy)
-  //   if trues.size != 1 || !rs.contains(trues.head) then
-  //     fail(
-  //       s"xx: ${xx}, yy: ${yy}: |${xx.show}, ${yy.show}| should satisfy one of ${rs.mkString("[", ",", "]")} relations, however it satisfies ${trues.mkString("[", ",", "]")} instead"
-  //     )
+  def assertOneOf[T: Domain](rs: Set[Rel])(xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
+    val trues = findRelations(xx, yy)
+    if trues.size != 1 || !rs.contains(trues.head) then
+      fail(
+        s"xx: ${xx}, yy: ${yy}: |${Show.asString(xx)}, ${Show
+            .asString(yy)}| should satisfy one of ${rs.mkString("[", ",", "]")} relations, however it satisfies ${trues.mkString("[", ",", "]")} instead"
+      )
 
-  // def assertOne[T: Ordering: Domain](r: Rel)(xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
-  //   val relations = relFnMap[T]
+  def assertOne[T: Ordering: Domain](r: Rel)(xx: Interval[T], yy: Interval[T])(using bOrd: Ordering[Mark[T]]): Unit =
+    val relations = relFnMap[T]
 
-  //   val fk = r
-  //   val bk = invRels(r)
-  //   val ks = List(fk, bk)
+    val fk = r
+    val bk = invRels(r)
+    val ks = List(fk, bk)
 
-  //   val fwdFn  = relations(fk)
-  //   val bckFn  = relations(bk)
-  //   val restFn = relations.filterNot { case (k, _) => ks.contains(k) }
+    val fwdFn  = relations(fk)
+    val bckFn  = relations(bk)
+    val restFn = relations.filterNot { case (k, _) => ks.contains(k) }
 
-  //   fwdFn(xx, yy) mustBe (true)
-  //   bckFn(yy, xx) mustBe (true)
+    fwdFn(xx, yy) mustBe (true)
+    bckFn(yy, xx) mustBe (true)
 
-  //   restFn.foreach { case (k, fn) =>
-  //     if fn(xx, yy) then fail(s"xx: ${xx}, yy: ${yy}; given that ${fk}|${xx.show}, ${yy.show}| == true; expected ${k}|${xx.show}, ${yy.show}| to be false, got true")
-  //     if fn(yy, xx) then fail(s"xx: ${xx}, yy: ${yy}; given that ${fk}|${xx.show}, ${yy.show}| == true; expected ${k}|${yy.show}, ${xx.show}| to be false, got true")
-  //   }
+    restFn.foreach { case (k, fn) =>
+      if fn(xx, yy) then
+        fail(
+          s"xx: ${xx}, yy: ${yy}; given that ${fk}|${Show.asString(xx)}, ${Show.asString(yy)}| == true; expected ${k}|${Show.asString(xx)}, ${Show.asString(yy)}| to be false, got true"
+        )
+      if fn(yy, xx) then
+        fail(
+          s"xx: ${xx}, yy: ${yy}; given that ${fk}|${Show.asString(xx)}, ${Show.asString(yy)}| == true; expected ${k}|${Show.asString(yy)}, ${Show.asString(xx)}| to be false, got true"
+        )
+    }
 
 object IntervalRelAssert extends IntervalRelAssert:
 
