f1f524c9f8b7f894d47199ecca6587fce416b2be
Grigorii Chudnov <g.chudnov@gmail.com>
2022-11-28 22:51:11 +0100

    upd svg

--- res/svgo.config.js
+++ res/svgo.config.js
@@ -66,12 +66,14 @@ function normalizeColors(ast, params, info) {
   const rChan = "a-fA-F0-9";
   const xRGB = new RegExp(`#([${rChan}])([${rChan}])([${rChan}])\\b`);
 
+  const xRRGGBB = new RegExp(`#([${rChan}]{6})\\b`);
+
   /**
    * Convert [r, g, b] to #rrggbb.
    */
   const rgbToHex = (r, g, b) => {
     const hexNumber = (r << 16) | (g << 8) | b;
-    const hexStr = hexNumber.toString(16).toUpperCase().padStart(6, "0");
+    const hexStr = hexNumber.toString(16).toLowerCase().padStart(6, "0");
     return "#" + hexStr;
   };
 
@@ -83,6 +85,11 @@ function normalizeColors(ast, params, info) {
             // #rgb -> #rrggbb
             value = value.replace(xRGB, (match, r, g, b) => {
               return "#" + r.repeat(2) + g.repeat(2) + b.repeat(2);
+            });
+
+            // #rrggbb -> #rrggbb
+            value = value.replace(xRRGGBB, (match) => {
+              return match.toLowerCase();
             });
 
             // rgb(a, b, c) => #rrggbb
@@ -169,19 +176,74 @@ function themeColors(ast, params, info) {
     "dark-primary": "#0d1117",
     "light-grey": "#f5f5f5", // grey
     "dark-grey": "#666666",
+    "light-grey-a": "#647687", // grey-a
+    "dark-grey-a": "#314354",
+    "light-grey-b": "#bac8d3", // grey-b
+    "dark-grey-b": "#23445d",
+    "light-grey-c": "#eeeeee", // grey-c
+    "dark-grey-c": "#36393d",
+    "light-grey-d": "#f9f7ed", // grey-d
+    "dark-grey-d": "#9e9d96",
     "light-blue": "#dae8fc", // blue
     "dark-blue": "#6c8ebf",
+    "light-blue-a": "#1ba1e2", // blue-a
+    "dark-blue-a": "#006eaf",
+    "light-blue-b": "#0050ef", // blue-b
+    "dark-blue-b": "#001dbc",
+    "light-blue-c": "#b1ddf0", // blue-c
+    "dark-blue-c": "#10739e",
+    "light-blue-d": "#cce5ff", // blue-d
+    "dark-blue-d": "#7e8e9e",
     "light-green": "#d5e8d4", // green
     "dark-green": "#82b366",
+    "light-green-a": "#60a917", // green-a
+    "dark-green-a": "#2d7600",
+    "light-green-b": "#008a00", // green-b
+    "dark-green-b": "#005700",
+    "light-green-c": "#6d8764", // green-c
+    "dark-green-c": "#3a5431",
+    "light-green-d": "#b0e3e6", // green-d
+    "dark-green-d": "#0e8088",
+    "light-green-e": "#cdeb8b", // green-e
+    "dark-green-e": "#7a8c53",
     "light-orange": "#ffe6cc", // orange
     "dark-orange": "#d79b00",
+    "light-orange-a": "#fa6800", // orange-a
+    "dark-orange-a": "#c73500",
+    "light-orange-b": "#ffcc99", // orange-b
+    "dark-orange-b": "#a38362",
     "light-yellow": "#fff2cc", // yellow
     "dark-yellow": "#d6b656",
+    "light-yellow-a": "#f0a30a", // yellow-a
+    "dark-yellow-a": "#bd7000",
+    "light-yellow-b": "#e3c800", // yellow-b
+    "dark-yellow-b": "#b09500",
+    "light-yellow-c": "#fad7ac", // yellow-c
+    "dark-yellow-c": "#b46504",
+    "light-yellow-d": "#ffff88", // yellow-d
+    "dark-yellow-d": "#8c8c4b",
     "light-red": "#f8cecc", // red
     "dark-red": "#b85450",
+    "light-red-a": "#a20025", // red-a
+    "dark-red-a": "#6f0000",
+    "light-red-b": "#e51400", // red-b
+    "dark-red-b": "#b20000",
+    "light-red-c": "#a0522d", // red-c
+    "dark-red-c": "#6d1f00",
+    "light-red-d": "#fad9d5", // red-d
+    "dark-red-d": "#ae4132",
     "light-violet": "#e1d5e7", // violet
     "dark-violet": "#9673a6",
-
+    "light-violet-a": "#6a00ff", // violet-a
+    "dark-violet-a": "#3700cc",
+    "light-violet-b": "#76608a", // violet-b
+    "dark-violet-b": "#432d57",
+    "light-violet-c": "#d0cee2", // violet-c
+    "dark-violet-c": "#56517e",
+    "light-rosa": "#d80073", // rosa
+    "dark-rosa": "#a50040",
+    "light-rosa-a": "#ffcccc", // rosa-a
+    "dark-rosa-a": "#947676",
     white: "#ffffff", // white
     black: "#000000", // black
   };
@@ -192,15 +254,47 @@ function themeColors(ast, params, info) {
     })
   );
 
+  if (Object.keys(colors).length !== Object.keys(values).length) {
+    console.log("ERROR: Colors.length is not matching Values.length, there are repeating values. Update the code to fix it.")
+  }
+
   const inversions = [
     "primary",
     "grey",
+    "grey-a",
+    "grey-b",
+    "grey-c",
+    "grey-d",
     "blue",
+    "blue-a",
+    "blue-b",
+    "blue-c",
+    "blue-d",
     "green",
+    "green-a",
+    "green-b",
+    "green-c",
+    "green-d",
+    "green-e",
     "orange",
+    "orange-a",
+    "orange-b",
     "yellow",
+    "yellow-a",
+    "yellow-b",
+    "yellow-c",
+    "yellow-d",
     "red",
+    "red-a",
+    "red-b",
+    "red-c",
+    "red-d",
     "violet",
+    "violet-a",
+    "violet-b",
+    "violet-c",
+    "rosa",
+    "rosa-a",
   ].reduce(
     (acc, name) => {
       return Object.assign(acc, {
@@ -216,7 +310,7 @@ function themeColors(ast, params, info) {
     const inversion = inversions[name];
     if (!inversion) {
       console.log(
-        `ERROR: Inversion of the color '${code}' is not defined. Update the script to define it.`
+        `ERROR: Inversion of the color '${code}' is not defined. Update the code to define it.`
       );
     }
     return inversion;
