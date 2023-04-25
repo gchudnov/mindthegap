93eb73416e2b6ba28a057f101212b085b27299d3
Grigorii Chudnov <g.chudnov@gmail.com>
2023-04-25 21:23:27 +0200

    add basic implementation

--- src/main/scala/com/github/gchudnov/mtg/internal/Split.scala
+++ src/main/scala/com/github/gchudnov/mtg/internal/Split.scala
@@ -6,10 +6,31 @@ import com.github.gchudnov.mtg.Domain
 
 private[mtg] object Split:
 
+  private case class Point[T](pt: Mark[T], i: Int)
+
   /**
    * A single split
    */
   private[mtg] final case class SplitState[T: Domain](interval: Interval[T], members: Set[Int])
+
+  /**
+   * Accumulator
+   */
+  private final case class AccState[T: Domain](splits: List[SplitState[T]], opens: Set[Int], last: Point[T]):
+
+    def withPoint(p: Point[T]): AccState[T] =
+      val it = Interval.make[T](last.pt, p.pt)
+      val ms = opens
+      val s  = SplitState(it, ms)
+
+      val os = if opens.contains(p.i) then opens - p.i else opens + p.i
+      val ls = p
+
+      this.copy(splits = splits ++ List(s), opens = os, last = ls)
+
+  private object AccState:
+    def init[T: Domain](p: Point[T]): AccState[T] =
+      AccState(splits = List.empty[SplitState[T]], opens = Set(p.i), last = p)
 
   /**
    * Split
@@ -17,7 +38,10 @@ private[mtg] object Split:
    * Split intervals into a collection of non-overlapping intervals (splits).
    */
   final def split[T: Domain](xs: Seq[Interval[T]])(using ordM: Ordering[Mark[T]]): List[Interval[T]] =
-    ???
+    val splits    = splitFind(xs)
+    val intervals = splits.map(_.interval)
+
+    intervals
 
   /**
    * SplitFind
@@ -25,4 +49,10 @@ private[mtg] object Split:
    * Split intervals into a collection of non-overlapping intervals (splits) and provide membership information.
    */
   final def splitFind[T: Domain](xs: Seq[Interval[T]])(using ordM: Ordering[Mark[T]]): List[SplitState[T]] =
-    ???
+    val ms = xs.zipWithIndex.flatMap(it => List(Point(it._1.left, it._2), Point(it._1.right, it._2))).sortWith { case (a, b) => ordM.lt(a.pt, b.pt) };
+
+    val acc = ms.foldLeft[Option[AccState[T]]](None) { case (acc, p) =>
+      acc.fold(Some(AccState.init(p)))(acc => Some(acc.withPoint(p)))
+    }
+
+    acc.fold(List.empty[SplitState[T]])(it => it.splits)
--- src/test/scala/com/github/gchudnov/mtg/internal/SplitSpec.scala
+++ src/test/scala/com/github/gchudnov/mtg/internal/SplitSpec.scala
@@ -35,4 +35,77 @@ final class SplitSpec extends TestSpec:
         actualInfo mustBe expectedInfo
       }
     }
+
+    /**
+     * {{{
+     *   [************************]             | [0,50]  : a
+     *        [*********]                       | [10,30] : b
+     *             [*******]                    | [20,35] : c
+     *                       [**]               | [40,45] : d
+     *                                 [****]   | [60,70] : e
+     *   [****]                                 | [0,10]  : s0
+     *        [****]                            | [10,20] : s1
+     *             [****]                       | [20,30] : s2
+     *                  [**]                    | [30,35] : s3
+     *                     [*]                  | [35,40] : s4
+     *                       [**]               | [40,45] : s5
+     *                          [*]             | [45,50] : s6
+     *                            [****]        | [50,60] : s7
+     *                                 [****]   | [60,70] : s8
+     * --+----+----+----+--+-+--+-+----+----+-- |
+     *   0   10   20   30 35   45     60   70   |
+     * 
+     * }}}
+     */
+    "several intervals" should {
+      "produce a split" in {
+        val a = Interval.closed(0, 50)  // [0, 50]
+        val b = Interval.closed(10, 30) // [10, 30]
+        val c = Interval.closed(20, 35) // [20, 35]
+        val d = Interval.closed(40, 45) // [40, 45]
+        val e = Interval.closed(60, 70) // [60, 70]
+
+        val s0 = Interval.closed(0, 10)
+        val g0 = Set(0)
+
+        val s1 = Interval.closed(10, 20)
+        val g1 = Set(0, 1)
+
+        val s2 = Interval.closed(20, 30)
+        val g2 = Set(0, 1, 2)
+
+        val s3 = Interval.closed(30, 35)
+        val g3 = Set(0, 2)
+
+        val s4 = Interval.closed(35, 40)
+        val g4 = Set(0)
+
+        val s5 = Interval.closed(40, 45)
+        val g5 = Set(0, 3)
+
+        val s6 = Interval.closed(45, 50)
+        val g6 = Set(0)
+
+        val s7 = Interval.closed(50, 60)
+        val g7 = Set.empty[Int]
+
+        val s8 = Interval.closed(60, 70)
+        val g8 = Set(4)
+
+        val input = List(a, b, c, d, e)
+
+        // [ ([0, 10], {0}), ([10, 20], {0, 1}), ([20, 30], {0, 1, 2}), ([30, 35], {0, 2}), ([35, 40], {0}), ([40, 45], {0, 3}), ([45, 50], {0}), ([50, 60], {}), ([60, 70], {4}) ]
+        val actual = Interval.splitFind(input)
+        val expected = List((s0, g0), (s1, g1), (s2, g2), (s3, g3), (s4, g4), (s5, g5), (s6, g6), (s7, g7), (s8, g8))
+
+        actual.size mustBe 9
+        actual mustBe expected
+
+
+        val actualIntervals = Interval.split(input)
+        val expectedIntervals = expected.map(_._1)
+
+        actualIntervals mustBe expectedIntervals
+      }
+    }
   }
