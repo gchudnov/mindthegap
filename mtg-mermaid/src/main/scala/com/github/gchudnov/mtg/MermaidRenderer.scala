dcf36dd6d97ce122750621c6439deadc2e613b33
Grigorii Chudnov <g.chudnov@gmail.com>
2024-08-14 23:40:55 +0200

    refactor

--- mtg-mermaid/src/main/scala/com/github/gchudnov/mtg/MermaidRenderer.scala
+++ mtg-mermaid/src/main/scala/com/github/gchudnov/mtg/MermaidRenderer.scala
@@ -4,12 +4,16 @@ import com.github.gchudnov.mtg.diagram.Diagram
 import com.github.gchudnov.mtg.diagram.Renderer
 
 /**
-  * Mermaid Renderer
-  * 
-  * Renders a diagram as a Mermaid diagram.
-  */
-final class MermaidRenderer extends Renderer {
+ * Mermaid Renderer
+ *
+ * Renders a diagram as a Mermaid diagram.
+ */
+private[mtg] final class MermaidRenderer extends Renderer:
   override def render(d: Diagram): Unit =
     ???
 
-}
+object MermaidRenderer:
+  def make(): MermaidRenderer =
+    new MermaidRenderer()
+
+// https://mermaid.js.org/syntax/gantt.html
