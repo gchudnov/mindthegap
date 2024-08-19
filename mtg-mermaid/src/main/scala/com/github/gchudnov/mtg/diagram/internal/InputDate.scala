10198d2ec607538b675e2cec875a380cfbe6cdf8
Grigorii Chudnov <g.chudnov@gmail.com>
2024-08-19 18:11:36 +0200

    upd docs

--- mtg-mermaid/src/main/scala/com/github/gchudnov/mtg/diagram/internal/InputDate.scala
+++ mtg-mermaid/src/main/scala/com/github/gchudnov/mtg/diagram/internal/InputDate.scala
@@ -3,6 +3,13 @@ package com.github.gchudnov.mtg.diagram.internal
 import java.time.*
 import com.github.gchudnov.mtg.diagram.internal.formatters.*
 
+/**
+ * InputDate
+ *
+ * Represents an input date that can be used in the MermaidJS diagram.
+ *
+ * To allow Domain type T to be used with mermadjs, you need to provide an instance of InputDate[T].
+ */
 private[mtg] trait InputDate[T]:
   /**
    * The pattern of the date for MermaidJS. Used to represent the input date in the diagram.
